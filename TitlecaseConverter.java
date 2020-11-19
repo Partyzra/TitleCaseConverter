353b865316766038f26c2d3b6847ab6c1c3a7fc7
Ryan <47995762+Partyzra@users.noreply.github.com>
2020-11-18 21:37:02 -0700

    Add files via upload

---
+++ TitlecaseConverter.java
@@ -0,0 +1,22 @@
+package com.sandermak;
+
+public class TitlecaseConverter {
+
+    // The extra kush function
+    public static String convertToTitleCase(String input) {
+        StringBuilder builder = new StringBuilder();
+
+        for (String word : input.split("\\s")) {
+            if (word.length() < 4) {
+                builder.append(word.toLowerCase());
+            } else {
+                builder.append(word.substring(0,1).toUpperCase());
+                builder.append(word.substring(1).toLowerCase());
+            }
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+
+}
